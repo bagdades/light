097303f0b13fb7be473441dbfeecbaf87bec8e5a
bagdades <volodumurkoval0@gmail.com>
2017-12-14 19:41:08 +0200

    First commit

---
+++ main.c
@@ -0,0 +1,21 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  14.12.17 19:37:52
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  vovan (), volodumurkoval0@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+int main(void) {
+	while(1);
+}
